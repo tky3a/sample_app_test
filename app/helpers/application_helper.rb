c09cc304ba91950f823c8437d369af1592fa4bc0
Takaya_Endo <takayamac@icloud.com>
2017-05-17 22:31:06 +0000

    Installed devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"               # 文字列を結合させてタイトルを完成させたものを返す
     end
     end
+    
+    def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
+    
